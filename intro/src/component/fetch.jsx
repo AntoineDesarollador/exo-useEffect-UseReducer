d63e5d214f4064d6660dac026ea46ce836c00037
Antoine <antoinejuillard015@gmail.com>
2023-05-11 10:30:50 -0500

    avancé fetch

--- intro/src/component/fetch.jsx
+++ intro/src/component/fetch.jsx
@@ -6,7 +6,12 @@ function jsonbase(){
 .then(data => {
     let product = data.products;
 for (let i = 0; i < product.length; i++) {
-  console.log(product[i]);
+ 
+  const para = document.createElement("p")
+  para.textContent = `titre ${product[i].title}`
+    para.value = [product[i].title,  product[i].description, product[i].category];
+    
+    console.log(para.value)
 }
 }
 
